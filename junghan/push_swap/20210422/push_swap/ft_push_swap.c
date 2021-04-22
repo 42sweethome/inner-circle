9bd6384d7ea5f8e3980c8de8153b1e56e4042b1e
junghan <42junghan@gmail.com>
2021-04-22 20:44:14 +0900

    making quick sort

---
+++ junghan/push_swap/20210422/push_swap/ft_push_swap.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_push_swap.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junghan <junghan@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/21 16:38:45 by junghan           #+#    #+#             */
+/*   Updated: 2021/04/22 16:47:16 by junghan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_push_swap.h"
+
+void push_swap(int ac, char **av)
+{
+	int	*stack_a;
+	int	*stack_b;
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	stack_a = 0;
+	stack_b = 0;
+	while (av[++i])
+	{
+		stack_a = input_int(stack_a, &len, av[i], ' ');
+	}
+	quick_sort(stack_a, stack_b, 0, len - 1);
+	
+	i = 0;
+	while (i < 10)
+		printf("%d\n", stack_a[i++]);
+}
+
+int	main(int ac, char **av)
+{
+	push_swap(ac, av);
+	return (0);
+}
