b04d0b605ad9e208c54159820d81fc87eacc9b47
junghan <42junghan@gmail.com>
2021-06-24 14:44:32 +0900

    erase theme

--- junghan/inception/hachiwatna/srcs/requirements/wordpress/tools/setup.sh
+++ junghan/inception/hachiwatna/srcs/requirements/wordpress/tools/setup.sh
@@ -9,7 +9,6 @@ if  [ ! -f /var/www/wordpress/wp-config.php ]; then
         
     done 
     wp core install --allow-root --url='junghan.42.fr' --title='WordPress for Inception' --admin_user=$WP_ID --admin_password=$WP_PW  --admin_email="junghan@student.42seoul.kr" --path='/var/www/wordpress';
-    wp  user create --allow-root $WP_USER_ID user2@user.com --user_pass=$WP_USER_PW --role=author
-    wp theme install --allow-root dark-mode --activate     
+    wp  user create --allow-root $WP_USER_ID user2@user.com --user_pass=$WP_USER_PW --role=author 
 fi 
 php-fpm7.3 --nodaemonize
