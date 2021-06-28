e6bcad2b23c91c9f033fc9b269088c01c62326dc
junghan <42junghan@gmail.com>
2021-06-28 12:35:56 +0900

    add grant privileges

--- junghan/inception/for_eval/srcs/requirements/mariadb/tools/setup.sh
+++ junghan/inception/for_eval/srcs/requirements/mariadb/tools/setup.sh
@@ -6,7 +6,8 @@ if [ ! -d "/var/lib/mysql/wordpress" ]; then
     service mysql start
     
     mysql -e "CREATE USER '${MARIA_ID}'@'localhost' identified by '${MARIA_PW}';" &&\
-	mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" &&\
+	mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" &&\]
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIA_ID}'@localhost IDENTIFIED BY '${MARIA_PW}';" &&\
 	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIA_ID}'@'%' IDENTIFIED BY '${MARIA_PW}';" &&\
 	mysql -e "UPDATE mysql.user SET Password = PASSWORD('${MARIA_PW}') WHERE User = 'root'" &&\
 	mysql -e "FLUSH PRIVILEGES;"
