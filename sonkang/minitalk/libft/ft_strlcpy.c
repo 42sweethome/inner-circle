2adef2df1a5aa181166fcad7b7cd5d065e947090
xhseb <zzzu5280@gmail.com>
2021-07-20 22:59:51 +0900

    minitalk

---
+++ sonkang/minitalk/libft/ft_strlcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sonkang <sonkang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/21 19:41:55 by sonkang           #+#    #+#             */
+/*   Updated: 2021/07/19 23:33:53 by sonkang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, char *src, size_t dest_size)
+{
+	size_t	i;
+	size_t	src_len;
+
+	if (!dest || !src)
+		return (0);
+	src_len = ft_strlen(src);
+	if (dest_size == 0)
+		return (src_len);
+	i = 0;
+	while (src[i] && i < src_len && i + 1 < dest_size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (src_len);
+}
