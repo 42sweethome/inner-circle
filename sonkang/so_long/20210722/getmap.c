63e9d1eba90f41cf3162c1078b716397ffe3b1e3
xhseb <zzzu5280@gmail.com>
2021-07-22 18:10:33 +0900

    20210722

--- sonkang/so_long/20210722/getmap.c
+++ sonkang/so_long/20210722/getmap.c
@@ -96,7 +96,7 @@ int		draw(t_info *info, int y, int x, t_data tex)
 		width = -1;
 		while (++width < tex.img_width)
 		{
-			printf("%d\n",tex.img_width * (x + info->map.col * (height + tex.img_height * y)) + width);
+			//printf("%d\n",tex.img_width * (x + info->map.col * (height + tex.img_height * y)) + width);
 			info->fimg.addr[/*tex.img_width * tex.img_height * (x + info->map.col * y) + (height * tex.img_width+ width)*/tex.img_width * (x + info->map.col * (height + tex.img_height * y)) + width] = tex.addr[height * tex.img_width + width];
 			
 		}
@@ -140,9 +140,22 @@ int		img_conv(t_info *info)
 //	mlx_put_image_to_window(info->win.mlx, info->win.mlx_win, info->tex[0].img, 0, 0);
 	return (0);
 }
-
+void	get_texture(t_info *info)
+{
+	info->tex[0].img = mlx_xpm_file_to_image(info->win.mlx, "./texture/grass.xpm", &info->tex[0].img_width, &info->tex[0].img_height);
+	info->tex[1].img = mlx_xpm_file_to_image(info->win.mlx, "./texture/water.xpm", &info->tex[1].img_width, &info->tex[1].img_height);
+	info->tex[2].img = mlx_xpm_file_to_image(info->win.mlx, "./texture/ghost.xpm", &info->tex[2].img_width, &info->tex[2].img_height);
+	info->tex[3].img = mlx_xpm_file_to_image(info->win.mlx, "./texture/grass.xpm", &info->tex[3].img_width, &info->tex[3].img_height);
+	info->tex[4].img = mlx_xpm_file_to_image(info->win.mlx, "./texture/ghost.xpm", &info->tex[4].img_width, &info->tex[4].img_height);
+	info->tex[0].addr = (int *)mlx_get_data_addr(info->tex[0].img, &info->tex[0].bits_per_pixel, &info->tex[0].line_length, &info->tex[0].endian);
+	info->tex[1].addr = (int *)mlx_get_data_addr(info->tex[1].img, &info->tex[1].bits_per_pixel, &info->tex[1].line_length, &info->tex[1].endian);
+	info->tex[2].addr = (int *)mlx_get_data_addr(info->tex[2].img, &info->tex[2].bits_per_pixel, &info->tex[2].line_length, &info->tex[2].endian);
+	info->tex[3].addr = (int *)mlx_get_data_addr(info->tex[3].img, &info->tex[3].bits_per_pixel, &info->tex[3].line_length, &info->tex[3].endian);
+	info->tex[4].addr = (int *)mlx_get_data_addr(info->tex[4].img, &info->tex[4].bits_per_pixel, &info->tex[4].line_length, &info->tex[4].endian);
+}
 int		show_snoop(t_info *info)
 {
+	get_texture(info);
 	img_conv(info);
 	/*info->data.img = mlx_xpm_file_to_image(info->win.mlx, "./snoopy.xpm", &(info->data.img_width), &(info->data.img_height));
 	info->data.addr = mlx_get_data_addr(info->data.img, &(info->data.bits_per_pixel),&(info->data.line_length), &(info->data.endian));
@@ -194,19 +207,6 @@ int	check_keypress(int key, t_info *info)
 	return (0);	
 }
 
-void	get_texture(t_info *info)
-{
-	info->tex[0].img = mlx_xpm_file_to_image(info->win.mlx, "./texture/grass.xpm", &info->tex[0].img_width, &info->tex[0].img_height);
-	info->tex[1].img = mlx_xpm_file_to_image(info->win.mlx, "./texture/water.xpm", &info->tex[1].img_width, &info->tex[1].img_height);
-	info->tex[2].img = mlx_xpm_file_to_image(info->win.mlx, "./texture/grass.xpm", &info->tex[2].img_width, &info->tex[2].img_height);
-	info->tex[3].img = mlx_xpm_file_to_image(info->win.mlx, "./texture/grass.xpm", &info->tex[3].img_width, &info->tex[3].img_height);
-	info->tex[4].img = mlx_xpm_file_to_image(info->win.mlx, "./texture/water.xpm", &info->tex[4].img_width, &info->tex[4].img_height);
-	info->tex[0].addr = (int *)mlx_get_data_addr(info->tex[0].img, &info->tex[0].bits_per_pixel, &info->tex[0].line_length, &info->tex[0].endian);
-	info->tex[1].addr = (int *)mlx_get_data_addr(info->tex[1].img, &info->tex[1].bits_per_pixel, &info->tex[1].line_length, &info->tex[1].endian);
-	info->tex[2].addr = (int *)mlx_get_data_addr(info->tex[2].img, &info->tex[2].bits_per_pixel, &info->tex[2].line_length, &info->tex[2].endian);
-	info->tex[3].addr = (int *)mlx_get_data_addr(info->tex[3].img, &info->tex[3].bits_per_pixel, &info->tex[3].line_length, &info->tex[3].endian);
-	info->tex[4].addr = (int *)mlx_get_data_addr(info->tex[4].img, &info->tex[4].bits_per_pixel, &info->tex[4].line_length, &info->tex[4].endian);
-}
 
 void	show_win(t_info *info)
 {
@@ -214,7 +214,7 @@ void	show_win(t_info *info)
 	info->win.mlx_win = mlx_new_window(info->win.mlx, 16 * info->map.col, 16 * info->map.row, "Hello world!");
 	info->fimg.img = mlx_new_image(info->win.mlx, 16 * info->map.col, 16 * info->map.row);
 	info->fimg.addr = (int *)mlx_get_data_addr(info->fimg.img, &info->fimg.bits_per_pixel, &info->fimg.line_length, &info->fimg.endian);
-	get_texture(info);
+	//get_texture(info);
 	mlx_loop_hook(info->win.mlx, show_snoop, info);
 	mlx_hook(info->win.mlx_win, 2, 1, check_keypress, info);
 	mlx_loop(info->win.mlx);
