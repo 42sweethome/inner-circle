c04a56b84caef630847193a1a95cb397ea698c26
xhseb <zzzu5280@gmail.com>
2021-07-27 17:24:52 +0900

    bonus complete

---
+++ sonkang/so_long/20210727/bonus/parsing_bonus.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doyun <doyun@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/19 10:58:24 by doyun             #+#    #+#             */
+/*   Updated: 2021/07/26 17:43:38 by sonkang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long_bonus.h"
+
+int	get_newline(char *argv, t_map *map_info)
+{
+	int		ret;
+	char	buf;
+	int		fd;
+
+	fd = open(argv, O_RDONLY);
+	if (fd == -1)
+		return (-1);
+	map_info->row = 0;
+	ret = read(fd, &buf, 1);
+	if (buf == '\n')
+		map_info->row++;
+	while (ret)
+	{
+		if (buf == '\n')
+			map_info->row++;
+		ret = read(fd, &buf, 1);
+	}
+	close(fd);
+	if (ret == -1)
+		return (ret);
+	return (map_info->row);
+}
+
+char	**map_parser(t_map *map_info, char *argv, int fd)
+{
+	int		idx;
+	int		ret;
+
+	map_info->row = get_newline(argv, map_info);
+	map_info->map = (char **)ft_calloc(map_info->row + 1, sizeof(char *));
+	if (!map_info->map)
+		return (NULL);
+	idx = -1;
+	ret = get_next_line(fd, &(map_info->map[++idx]));
+	map_info->col = ft_strlen(map_info->map[idx]);
+	while (ret)
+	{
+		ret = get_next_line(fd, &(map_info->map[++idx]));
+		map_info->temp_col = ft_strlen(map_info->map[idx]);
+		if (idx == map_info->row - 1)
+			break ;
+		if (map_info->col != map_info->temp_col)
+			return (NULL);
+	}
+	if (ret == -1)
+		return (NULL);
+	return (map_info->map);
+}
+
+int	parsing(t_info *info, char *argv)
+{
+	int		check;
+	int		fd;
+
+	info->map.walk = 0;
+	info->map.way = 4;
+	check = check_extention(argv);
+	if (check == -1)
+		return (print_error());
+	fd = open(argv, O_RDONLY);
+	if (fd == -1)
+		return (print_error());
+	info->map.map = map_parser(&info->map, argv, fd);
+	if (!info->map.map)
+		return (print_error());
+	check = check_map_valid(&info->map);
+	if (check == -1)
+		return (print_error());
+	check = check_parm_valid(&info->map);
+	if (check == -1)
+		return (print_error());
+	info->map.v_d = 0;
+	return (0);
+}
