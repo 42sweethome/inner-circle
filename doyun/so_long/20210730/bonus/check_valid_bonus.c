9fdf6a450474417be93ba51fd6407714b37b04a1
yundonghwan <doyun@yundonghwanui-MacBookPro.local>
2021-07-30 10:16:45 +0900

    42

---
+++ doyun/so_long/20210730/bonus/check_valid_bonus.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_valid_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doyun <doyun@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/19 14:58:39 by doyun             #+#    #+#             */
+/*   Updated: 2021/07/28 18:48:10 by sonkang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long_bonus.h"
+
+int	check_extention(char *argv)
+{
+	int		len;
+	int		ret;
+
+	len = ft_strlen(argv);
+	if (len >= 5)
+		ret = ft_strncmp(&(argv[len - 4]), ".ber", 4);
+	else
+		return (-1);
+	if (ret != 0)
+		return (-1);
+	return (0);
+}
+
+void	get_player_position(int *count, t_map *map, int row, int col)
+{
+	*count += 1;
+	map->player_x = col;
+	map->player_y = row;
+}
+
+void	get_villian_position(t_map *map, int row, int col)
+{
+	map->v_x = col;
+	map->v_y = row;
+}
+
+void	get_collect_ea(int *count, t_map *map/*,int row, int col*/)
+{
+	*count += 1;
+	map->collect = *count;
+	map->v_f = *count;
+}
+
+int	check_parm_valid(t_map *m_info)
+{
+	int			row;
+	int			col;
+	static int	count[3];
+
+	row = -1;
+	while (m_info->map[++row])
+	{
+		col = -1;
+		while (m_info->map[row][++col])
+		{
+			if (m_info->map[row][col] == '0' || m_info->map[row][col] == '1')
+				continue ;
+			else if (m_info->map[row][col] == 'C')
+				get_collect_ea(&count[0], m_info/*, row, col*/);
+			else if (m_info->map[row][col] == 'E')
+				count[1]++;
+			else if (m_info->map[row][col] == 'P')
+				get_player_position(&count[2], m_info, row, col);
+		//	else if (m_info->map[row][col] == 'V')
+		//		get_villian_position(&count[3], m_info, row, col);
+			else
+				return (-1);
+		}
+	}
+	if (count[0] < 1 || count[1] != 1 || count[2] != 1)
+		return (-1);
+	return (0);
+}
+
+int	check_map_valid(t_map *map_info)
+{
+	int	row;
+	int	col;
+
+	row = 0;
+	col = -1;
+	while ((map_info->map)[row][++col])
+	{
+		if (map_info->map[row][col] != '1')
+			return (-1);
+		if (map_info->map[map_info->row - 1][col] != '1')
+			return (-1);
+	}
+	while (++row < map_info->row - 1)
+	{
+		if (map_info->map[row][0] != '1' || map_info->map[row][col - 1] != '1')
+			return (-1);
+	}
+	return (0);
+}
---
+++ doyun/so_long/20210730/bonus/operate_bonus.c
@@ -0,0 +1,144 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operate_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doyun <doyun@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/19 10:58:24 by doyun             #+#    #+#             */
+/*   Updated: 2021/07/28 18:51:59 by sonkang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long_bonus.h"
+
+void	ft_exit(t_info *info)
+{
+	if (info->map.collect == 0)
+	{
+		printf("well done\n");
+		exit(0);
+	}
+	else
+		printf("keep going!\n");
+}
+
+void	die(void)
+{
+	printf("you die...\n");
+	exit(0);
+}
+
+void	move(t_info *info, int x, int y)
+{
+	if (info->map.map[info->map.player_y + y][info->map.player_x + x] != '1')
+	{
+		if (info->map.map[info->map.player_y + y] \
+			[info->map.player_x + x] == 'V')
+			die();
+		if (info->map.map[info->map.player_y + y] \
+			[info->map.player_x + x] == 'E')
+			ft_exit(info);
+		if (info->map.map[info->map.player_y + y] \
+			[info->map.player_x + x] == 'C')
+		{
+			info->map.collect -= 1;
+			if (info->map.collect == info->map.v_f - 1)
+				get_villian_position(&info->map, info->map.player_y + y, info->map.player_x + x);
+		}
+		if (!(info->map.map[info->map.player_y + y] \
+			[info->map.player_x + x] == 'E' && info->map.collect != 0))
+		{
+			info->map.map[info->map.player_y][info->map.player_x] = '0';
+			info->map.map[info->map.player_y += y] \
+			[info->map.player_x += x] = 'P';
+			info->map.walk++;
+		}
+	}
+}
+
+int	check_keypress(int key, t_info *info)
+{
+	if (key == 13)
+	{
+		move(info, 0, -1);
+		info->map.way = 11 + (info->map.walk % 2);
+	}
+	else if (key == 1)
+	{
+		move(info, 0, 1);
+		info->map.way = 13 + (info->map.walk % 2);
+	}
+	else if (key == 0)
+	{
+		move(info, -1, 0);
+		info->map.way = 9 + (info->map.walk % 2);
+	}
+	else if (key == 2)
+	{
+		move(info, 1, 0);
+		info->map.way = 7 + (info->map.walk % 2);
+	}
+	else if (key == 53)
+		exit(0);
+	return (0);
+}
+
+int	check_button(void)
+{
+	exit(0);
+	return (0);
+}
+
+void	patrol(t_info *info, int i, int l)
+{
+	info->map.map[info->map.v_y][info->map.v_x] = '0';
+	info->map.map[info->map.v_y][info->map.v_x += i] = 'V';
+	info->map.v_d = 0;
+	if (l == 1)
+		die();
+}
+
+void	collectible(t_info *info)
+{
+	static int	i;
+
+	if (i++ >= 10)
+	{
+		if (info->map.c < 5)
+			info->map.c++;
+		else
+			info->map.c = 2;
+		i = 0;
+	}
+}
+
+void	villain(t_info *info)
+{
+	static int	i;
+	static int	check;
+
+	if (i++ >= 100)
+	{
+		if (check == 0)
+		{
+			if (info->map.map[info->map.v_y][info->map.v_x + 1] == '0')
+				patrol(info ,1 ,0);
+			else if (info->map.map[info->map.v_y][info->map.v_x + 1] == 'P')
+				patrol(info, 1, 1);
+			else
+				check = 1;
+		}
+		if (check == 1)
+		{
+			if (info->map.map[info->map.v_y][info->map.v_x - 1] == '0')
+				patrol(info, -1, 0);
+			else if (info->map.map[info->map.v_y][info->map.v_x - 1] == 'P')
+				patrol(info, -1, 1);
+			else
+				check = 0;
+		}
+		i = 0;
+		info->map.v_d = check;
+	}
+}
