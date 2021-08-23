10cc989067a94e7c1942630282cdf57fc0074b18
한중명 <gkswndaud@gmail.com>
2021-08-23 18:20:28 +0900

    & export

---
+++ junghan/minishell/20210819/libft/ft_strmapi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daekim <daekim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/28 10:53:26 by daekim            #+#    #+#             */
+/*   Updated: 2021/08/02 09:02:50 by daekim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char *s, char (*f)(unsigned int, char))
+{
+	char			*new;
+	unsigned int	i;
+
+	if (s == 0)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	new = malloc(sizeof(char) * (i + 1));
+	if (!new)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		new[i] = f(i, s[i]);
+		i++;
+	}
+	new[i] = 0;
+	return (new);
+}
