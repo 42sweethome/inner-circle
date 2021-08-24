1332ad82101f79b4eae9f82cd2e73606dce01b03
김대현 <dhkim5608@naver.com>
2021-08-24 15:04:33 +0900

    ch_enving

---
+++ daekim/minishell/20210823/command2.c
@@ -0,0 +1,108 @@
+#include "minishell.h"
+
+char	**copy_arr(char **envp)
+{
+	char **sort_arr;
+	int idx;
+	int size;
+
+	size = -1;
+	while (envp[++size])
+		;
+	sort_arr = (char **)ft_calloc(size + 1, sizeof(char *));
+	if (sort_arr == 0)
+		return (0);
+	idx = -1;
+	while (++idx < size)
+	{
+		sort_arr[idx] = ft_strdup(envp[idx]);
+		if (sort_arr[idx] == 0)
+		{
+			ft_free(sort_arr);
+			return (0);
+		}
+	}
+	return (sort_arr);
+}
+
+void	swap(char **str1, char **str2)
+{
+	char *tmp;
+
+	tmp = *str1;
+	*str1 = *str2;
+	*str2 = tmp;
+}
+
+void	print_env(char **sort_arr)
+{
+	int idx;
+	int jdx;
+	int v_flag;
+
+	idx = -1;
+	while (sort_arr[++idx])
+	{
+		printf("declare -x ");
+		v_flag = 0;
+		jdx = -1;
+		while (sort_arr[idx][++jdx])
+		{
+			printf("%c", sort_arr[idx][jdx]);
+			if ((v_flag == 0 && sort_arr[idx][jdx] == '=') || \
+					(sort_arr[idx][jdx + 1] == 0 && v_flag == 1))
+			{
+				v_flag = 1;
+				printf("\"");
+			}
+		}
+		printf("\n");
+	}
+
+	printf("---------------------\n");
+	idx = -1;
+	while (sort_arr[++idx])
+	{
+		printf("declare -x %s\n", sort_arr[idx]);
+	}
+}
+
+int	show_env(t_mini *mini, char **envp)
+{
+	char **sort_arr;
+	int	idx;
+	int	jdx;
+	int	cmp;
+
+	sort_arr = copy_arr(envp);
+	if (sort_arr == 0)
+		return (mini->err.malloc);
+	idx = -1;
+	while (sort_arr[++idx])
+	{
+		jdx = idx;
+		while (sort_arr[++jdx])
+		{
+			cmp = ft_strncmp(sort_arr[idx], sort_arr[jdx], \
+					ft_strlen(sort_arr[idx]));
+			if (cmp > 0)
+				swap(&sort_arr[idx], &sort_arr[jdx]);
+		}
+	}
+	print_env(sort_arr);
+	return (0);
+}
+
+int	ft_export(t_mini *mini, /*char *cmd,*/ char **envp)
+{
+	int idx;
+
+	idx = mini->first;
+	if (mini->buf[++idx] == 0)
+	{
+		if (show_env(mini, envp) == mini->err.malloc)
+			return (mini->err.malloc);
+	}
+
+	return (0);
+}
---
+++ daekim/minishell/20210824/minishell.h
@@ -0,0 +1,54 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <errno.h>
+# include <stdlib.h>
+//# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft/libft.h"
+# include "gnl/get_next_line.h"
+
+typedef	struct s_err
+{
+	int			malloc;
+	int			cmd;
+	int			quo;
+	int			argv;
+}			t_err;
+
+typedef struct s_mini
+{
+	char		**buf;
+	char		***envp;
+	char		**path;
+	int			s_quo;
+	int			d_quo;
+	int			cnt_quo;
+	int			cnt_rvslash;
+	int			odd_quo;
+	int			env_len;
+	int			dollar;
+	int			first;
+	t_err		err;
+}			t_mini;
+
+int		space_split(char *s, char c, t_mini *mini);
+int		check_quo(char *s, char c, int i, t_mini *mini);
+int		quo_while(char *s, char c, t_mini *mini, int i);
+int		case_quo(char *str, int *idx, t_mini *mini);
+int		cmd_err(char *cmd, int err_num, t_mini *mini);
+int		check_cmd(char *cmd, t_mini *mini, char ***envp);
+char	ft_free(char **new);
+int		al_num_under(int c);
+int		check_env(char *env_str, int i, t_mini *mini);
+int		copy_env(char *new, char *env_str, int i, t_mini *mini);
+void	ft_echo(t_mini *mini);
+void	ft_pwd(void);
+void	ft_env(char ***envp);
+void	ft_chdir(t_mini *mini);
+int		ft_export(t_mini *mini, char ***envp);
+
+#endif
