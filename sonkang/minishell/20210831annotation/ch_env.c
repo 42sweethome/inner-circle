72f9feab5014d5b1d2faf306459067f8e036386c
xhseb <zzzu5280@gmail.com>
2021-08-31 23:54:56 +0900

    add minishell 20210831 annotation version

---
+++ sonkang/minishell/20210831annotation/ch_env.c
@@ -0,0 +1,86 @@
+#include "minishell.h"
+
+int	search_env(t_mini *mini, char **env, char *tmp, int len)
+{
+	int	idx;
+	int	cmp;
+
+	*env = getenv(tmp);
+	if (*env == 0)
+	{
+		idx = -1;
+		while ((*mini->envp)[++idx])
+		{	
+			cmp = ft_strncmp((*mini->envp)[idx], tmp, len);
+			if (cmp == 0)
+			{
+				if ((*mini->envp)[idx][len] == '=')
+				{
+					*env = ft_strdup(&(*mini->envp)[idx][len + 1]);
+					if (*env == 0)
+						return (mini->err.malloc);
+					break ;
+				}
+			}
+		}
+		if ((*mini->envp)[idx] == 0)
+			mini->env_flag = 1;
+	}
+	return (0);
+}
+
+int	copy_env(char *new, char *env_str, int i, t_mini *mini)
+{
+	char	*env;
+	char	*tmp;
+	int		len;
+	int		idx;
+	int		ret;
+
+	len = 0;
+	while (al_num_under(env_str[i + len + 1]))
+		len++;
+	if (len == 0)
+		return (i);
+	tmp = ft_calloc(len + 1, sizeof(char));
+	if (tmp == 0)
+		return (mini->err.malloc);
+	idx = -1;
+	while (++idx < len)
+			tmp[idx] = env_str[i + idx + 1];
+	ret = search_env(mini, &env, tmp, len);
+	if (ret == mini->err.malloc)
+		return (mini->err.malloc);
+	ft_strlcpy(new, env, ft_strlen(env) + 1);
+	if (i != 0 && env_str[i - 1] != ' ' && env_str[i - 1] != '|' && mini->env_flag == 1)
+		mini->env_flag = 0;
+	return (i + len + 1);
+}
+
+int	check_env(char *env_str, int i, t_mini *mini)
+{
+	char	*tmp;
+	char	*env;
+	int		len;
+	int		idx;
+	int		ret;
+
+	len = 0;
+	while (al_num_under(env_str[i + len + 1]))
+		len++;
+	tmp = ft_calloc(len + 1, sizeof(char));
+	if (tmp == 0)
+		return (mini->err.malloc);
+	idx = -1;
+	while (++idx < len)
+		tmp[idx] = env_str[i + idx + 1];
+	ret = search_env(mini, &env, tmp, len);
+	if (ret == mini->err.malloc)
+		return (mini->err.malloc);
+	mini->env_len = mini->env_len + ft_strlen(env) - len;
+	if (len != 0)
+		mini->dollar++;
+	if (i != 0 && env_str[i - 1] != ' ' && env_str[i - 1] != '|' && mini->env_flag == 1)
+			mini->env_flag = 0;
+	return (i + len);
+}
---
+++ sonkang/minishell/20210831annotation/ch_space1.c
@@ -0,0 +1,175 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ch_space1.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sonkang <sonkang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/31 09:05:42 by daekim            #+#    #+#             */
+/*   Updated: 2021/08/31 23:49:26 by sonkang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	scpy(char *new, char *str, size_t end, t_mini *mini)
+{
+	size_t		start;
+	int			idx;
+
+	start = 0;
+	idx = 0;
+	mini->s_quo = 0;
+	mini->d_quo = 0;
+
+	while (start < end)
+	{
+		if (case_quo(str, &idx, mini))
+			continue ;
+		if (mini->d_quo == 1 && str[idx] == '\\')
+			idx++;
+		if (mini->s_quo == 0 && str[idx] == '$')
+		{
+			idx = copy_env(&new[start], str, idx, mini);
+			if (idx == mini->err.malloc)
+				return (mini->err.malloc);
+			if (new[start] == 0 && mini->d_quo == 0)
+				continue ;
+			if (new[start] != 0)
+			{
+				while (new[start])
+					start++;
+				continue ;
+			}
+		}
+		new[start++] = str[idx++];
+	}
+	return (0);
+}
+
+static int	spliting(char *s, char space, char **new, t_mini *mini)
+{
+	size_t		i;
+	size_t		count;
+	size_t		start;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{			
+		if (s[i] && s[i] != space && s[i] != '|')
+		{
+			mini->cnt_rvslash = 0;
+			mini->cnt_quo = 0;
+			mini->env_len = 0;
+			mini->dollar = 0;
+			mini->env_flag = 0;
+			start = i;
+			i = quo_while(s, space, mini, i); //countc와 동일한 작업
+			new[count] = (char *)ft_calloc((i - start + 1 - mini->cnt_rvslash \
+						- mini->cnt_quo + mini->env_len - mini->dollar), sizeof(char)); //역슬래쉬와 따옴표의 갯수만큼 적게할당
+			if (!new[count] || i == (size_t)mini->err.malloc)
+				return (mini->err.malloc);
+			if (scpy(new[count], &s[start], (i - start - mini->cnt_rvslash \
+						- mini->cnt_quo + mini->env_len - mini->dollar), mini))//구분된 문자열을 new라는 이중배열에 넣어줌
+				return (mini->err.malloc);
+			if (mini->env_flag == 0)
+				count++;
+			else
+			{
+				free(new[count]);
+				new[count] = 0;
+			}
+
+		}
+		else if (s[i] == space)
+			i++;
+		else if (s[i] == '|')
+		{
+			new[count] = ft_strdup("|");
+			if (new[count] == 0)
+				return (mini->err.malloc);
+			i++;
+			count++;
+		}
+	}
+/*	int j = -1;
+	while (new[++j])
+	{
+		printf("new : %s\n",new[j]);
+	}*/
+	return (0);
+}
+
+static size_t	countc(char *s, char space, t_mini *mini) //문자열의 총 길이를 리턴하기 위한 함수
+{
+	size_t		i;
+	size_t		count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] && s[i] != space && s[i] != '|') 
+		{
+			mini->env_flag = 0;
+			count++;
+			i = quo_while(s, space, mini, i); // 내부적으로 큰따옴표와 작은따옴표에 관한 분류작업을함
+			//if ((int)i == -2)		// ex) 따옴표의 갯수, 역슬래시 파악 등
+			if (mini->env_flag == 1)
+				count--;
+			if (i == (size_t)mini->err.malloc)
+				return (mini->err.malloc);
+		}
+		else if (s[i] == space)
+			i++;
+		else if (s[i] == '|')
+		{
+			i++;
+			mini->pipe++;
+		}
+	}
+	return (count + mini->pipe);
+}
+
+int	check_pipe_pos(t_mini *mini)
+{
+	int idx;
+
+	idx = -1;
+	if (!ft_strncmp(mini->buf[0], "|", 2))
+		return (mini->err.pipe);
+	while(mini->buf[++idx] != NULL)
+	{
+		if (!ft_strncmp(mini->buf[idx], "|", 2))
+		{
+			if (mini->buf[idx + 1] == NULL)
+				return (mini->err.pipe);
+			else if (!ft_strncmp(mini->buf[idx + 1], "|", 2))
+				return (mini->err.pipe);
+		}
+	}
+	return (0);
+}
+
+int	space_split(char *s, char space, t_mini *mini)
+{
+	size_t		num;
+
+	//if (s == 0)
+	//	return (mini->err.malloc); parsing에서 only_space로 처리해줌
+	num = countc(s, space, mini);
+	if (num == 0) //$abc ""구분
+	{
+		ft_free(mini->path);
+		return (0);
+	}
+	mini->buf = (char **)ft_calloc((num + 1), sizeof(char *));
+	if (!mini->buf || num == (size_t)mini->err.malloc) //num -1일때 여기서 에러처리하는게 맞는지
+		return (mini->err.malloc);
+	if (spliting(s, space, mini->buf, mini))
+		return (mini->err.malloc);
+	if (check_pipe_pos(mini) == mini->err.pipe)
+		return (mini->err.pipe);
+	return (1);
+}
