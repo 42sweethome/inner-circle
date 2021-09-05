a50b32f8fba07eb50d828db0e40a214bcead293e
김대현 <dhkim5608@naver.com>
2021-09-05 20:21:41 +0900

    so_long_git review

--- daekim/so_long/so_long_git/so_long.c
+++ daekim/so_long/so_long_git/so_long.c
@@ -14,7 +14,7 @@
 
 void	print_error(int i)
 {
-	printf("Error\n");
+	printf("Error : ");
 	if (i == 0)
 		printf("number of arguments isn't right\n");
 	if (i == 1)
@@ -37,24 +37,17 @@ void	show_win(t_info *info)
 	int	win_x;
 	int	win_y;
 
-	printf("2.5\n");
 	info->win.mlx = mlx_init();
-	printf("3\n");
 	mlx_get_screen_size(info->win.mlx, &win_x, &win_y);
-	printf("4\n");
 	if (!(win_x >= 64 * info->map.col && win_y >= \
 		64 * info->map.row))
 		print_error(6);
-	printf("5\n");
 	info->win.mlx_win = mlx_new_window(info->win.mlx, 64 * info->map.col, \
 	64 * info->map.row, "Maply story");
-	printf("6\n");
 	info->fimg.img = mlx_new_image(info->win.mlx, 64 * info->map.col, \
 	64 * info->map.row);
-	printf("7\n");
 	info->fimg.addr = (unsigned int *)mlx_get_data_addr(info->fimg.img, \
 	&info->fimg.bits_per_pixel, &info->fimg.line_length, &info->fimg.endian);
-	printf("before get\n");
 	get_texture(info);
 	printf("how many walk : 0\n");
 	mlx_hook(info->win.mlx_win, 2, 0, check_keypress, info);
@@ -69,9 +62,7 @@ int	main(int argc, char **argv)
 
 	if (argc == 2)
 	{
-		printf("1\n");
 		parsing(&info, argv[1]);
-		printf("2\n");
 		show_win(&info);
 	}
 	else
