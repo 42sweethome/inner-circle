7392d005465d0f69b0185e061b667d58c93eebc6
YunDongHwan <flfns333@gmail.com>
2021-10-25 18:04:48 +0900

    COMMIT END --- Feat : make cpp00 ex00

---
+++ cpp/cpp00/ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctype.h>
+
+int	main(int argc, char **argv)
+{
+	int	idx;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	idx = 0;
+	while (++idx < argc)
+	{
+		for (int i = 0; argv[idx][i] != 0; i++)
+		{
+			std::cout << (char)(toupper(argv[idx][i]));
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
