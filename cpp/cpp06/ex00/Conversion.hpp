7523ab9373312f7710470356cf9d19a6062f41c6
gkswndaud <gkswndaud@gmail.com>
2021-11-19 17:56:26 +0900

    Feat : CPP06 module ex00
    Exercise 00: Scalar conversion
    
     argument 들어온 값이 어떤 유형인지 확인하기
     type체크 후, 형변환
    
     #17

--- cpp/cpp06/ex00/Conversion.hpp
+++ cpp/cpp06/ex00/Conversion.hpp
@@ -1,7 +1,25 @@
 #ifndef CONVERSION_HPP
 # define CONVERSION_HPP
 
-#include <iostream>
+# include <iostream>
+# include <string>
+# include <cctype>
+# include <cstdlib>
+# include <climits>
+# include <iomanip>
+
+ typedef struct s_info {
+	int sign;
+	int point;
+	int isfloat;
+	int pseudo; //
+	int isstring; //
+	int ischar; //
+	int isprint; //
+	int p_zero;
+	double dounum;
+} t_info;
+
 
 class conversion
 {
