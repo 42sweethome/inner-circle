581dcc71a6de1e98a477f51feff09fa75e53a7c1
YunDongHwan <flfns333@gmail.com>
2021-11-30 17:59:25 +0900

    Feat : CPP Module 07 ex00

---
+++ cpp/cpp07/ex00/Function.hpp
@@ -0,0 +1,18 @@
+#ifndef FUNCTION_HPP
+# define FUNCTION_HPP
+
+#include <iostream>
+
+class function
+{
+
+};
+
+//template <class function>
+void swap(function &a, function &b); //왜 &로 받아야 하고 class 로 다른 자료형을 받는 template구현되는가?
+//template <class function>
+function min(function a, function b);
+//template <class function>
+function max(function a, function b);
+
+#endif
